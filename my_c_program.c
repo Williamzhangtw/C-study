2b66f6a1acc49e857d31176ff40b394fb7f96a9e
root <root@DESKTOP-0V9K0CT.localdomain>
2018-04-18 01:08:34 -0700

    select funtions

--- my_c_program.c
+++ my_c_program.c
@@ -1,29 +1,55 @@
 #include <stdio.h>
 
-int main()
-{
+void Select_Your_Choice(){
+  printf("********\n");
+  printf("please select your function\n");
+  printf("1)add\n");
+  printf("2)multiply\n");
+  printf("q)quit\n");
+}
 
-  printf("hello, this program is used to test how positive or negative the integer numbers are\n");
+void Add(){
+  printf("please input integer a\n");
+  int a,b;
+  scanf("%i",&a);
+  printf("please input integer b\n");
+  scanf("%i",&b);
+  printf("a+b = %i\n",a+b);
+}
 
-  int integer = 0;
-  
-  scanf("%i",&integer);
+void Multiply(){
+  printf("please input integer a\n");
+  int a,b;
+  scanf("%i",&a);
+  printf("please input integer b\n");
+  scanf("%i",&b);
+  printf("a*b = %i\n",a*b);
+}
 
-  if(integer > 999){
-    printf("%i is highly positive\n",integer);
-  }
-  else if((integer<=999)&&(integer>100))
-    printf("%i is very positive\n",integer);
-  else if((integer>0)&&(integer<=100))
-    printf("%i is positive\n",integer);
-  else if(integer==0)
-    printf("integer is 0\n");
-  else if((integer>=-100)&&(integer<0))
-    printf("%i is negative\n",integer);
-  else if((integer<-100)&&(integer>=-999))
-    printf("%i is very negative\n",integer);
-  else
-    printf("%i is highly negative\n",integer);
-  
+int main(){
+
+  printf("hello, this program is used to select functions\n");
+  while(1) {
+  Select_Your_Choice();
+  char choice = 0;
+
+  scanf("%c",&choice);
+
+    switch(choice){
+    case '1':
+      Add();
+      break;
+    case '2':
+      Multiply();
+      break;
+    case 'q':
+      return 0;
+      break;
+    default:
+      printf("no this option\n");
+      break;
+    }
+
+    }
   return 0;
 }
